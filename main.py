e1a97454a63848e7691322416d9b1ac2b65a5de5
Sameen-Iqbal <Sameen.iqbal@city.ac.uk>
2025-09-20 00:38:58 +0100

    intialising a free resource(small) model to generate ai content

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-from gpt4all import GPT4ALL
 
-model = GPTALL("")
+from gpt4all import GPT4All
+
+model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
+
+
+def prompt():
+    prompt = input("Please enter prompt: ")
+    return prompt
+
+def generate(prompt):
+    with model.chat_session():
+        response = model.generate(prompt, max_tokens=150)
+        print("Generating answers...")
+        print(response)
+
+
+generate(prompt())
+
